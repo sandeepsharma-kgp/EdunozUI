e66c5eae49c77bc50ccdfe70fe7e7ceab3ed5543
gauravtirodkar <gauravtirodkar42@gmail.com>
2019-07-13 00:07:53 +0530

    Navbar and Carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Navbar />
+    <VisionCarousel fluid alt="Responsive image"/>
+    <Programs />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
+import VisionCarousel from "@/components/VisionCarousel.vue";
+import Programs from "@/components/Programs.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Navbar,
+    VisionCarousel,
+    Programs
   }
 };
 </script>
