1fbb59b6017eb08f0f4f644880462f8cd0943435
Shrikant S. C <chambharess16.it@coep.ac.in>
2019-07-17 00:13:19 +0530

    carousel working fine

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <nav-bar></nav-bar>
+    <vision-carousel></vision-carousel>
+    <webinar></webinar>
     <card-bar></card-bar>
     <popular-tech></popular-tech>
     <our-branches></our-branches>
@@ -15,6 +18,10 @@ import popularTech from "../components/Home/popular-tech.vue";
 import ourBranches from "../components/Home/our-branches.vue";
 import moreAbout from "../components/Home/more-about-us.vue";
 import footerBar from "../components/Home/footer.vue";
+import Navbar from "../components/Home/Navbar.vue";
+import VisionCarousel from "../components/Home/VisionCarousel.vue";
+import Webinar from "../components/Home/Webinar.vue";
+
 export default {
   name: "home",
   components: {
@@ -22,7 +29,10 @@ export default {
     "popular-tech": popularTech,
     "our-branches": ourBranches,
     "more-about": moreAbout,
-    "footer-bar": footerBar
+    "footer-bar": footerBar,
+    "nav-bar": Navbar,
+    "vision-carousel": VisionCarousel,
+    "webinar": Webinar
   }
 };
 </script>
