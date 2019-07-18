f2d3833a4925e3024a0898460f4a6b86187f69bf
Shrikant S. C <chambharess16.it@coep.ac.in>
2019-07-18 11:44:12 +0530

    vuetify added

--- src/components/Home/Webinar.vue
+++ src/components/Home/Webinar.vue
@@ -1,17 +1,18 @@
 <template>
-  <div style="background-color:#eceef8; position:relative;">
-    <div class="clearfix">
-      <b-img left :src="require('../../assets/images/blur-close-up-desk-433604.jpg')" alt="Left image" style="width:600px; height:405px; padding:15px 40px; border-radius:45px;"></b-img>
-     <div style="position:absolute; top:35%; left:55%; width:500px;">
+  <b-row style="background-color: #eceef8;
+ padding: 24px 0 24px 0;">
+    <b-col align-self="center">
+      <b-img :src="require('../../assets/images/blur-close-up-desk-433604.jpg')" alt="Left image" style="width: 90%; height: 400px; border-radius:24px;"></b-img>
+    </b-col>
+     <b-col lg align-self="center">
       <h4>Webinar</h4>
       <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. 
         The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of 
         Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.
       </p>
       <b-btn >See Details</b-btn>
-     </div>
-    </div>
-  </div>
+     </b-col>
+    </b-row>
 </template>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,13 @@
 <template>
-  <div class="home">
-    <nav-bar></nav-bar>
-    <carousel-vis></carousel-vis>
-    <webinar></webinar>
-    <card-bar></card-bar>
-    <popular-tech></popular-tech>
-    <our-branches></our-branches>
-    <more-about></more-about>
-    <footer-bar></footer-bar>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import cardBar from "../components/Home/card-bar.vue";
-import Carousel from "../components/Home/Carousel.vue";
+  import HelloWorld from '../components/HelloWorld'
 
-import popularTech from "../components/Home/popular-tech.vue";
-import ourBranches from "../components/Home/our-branches.vue";
-import moreAbout from "../components/Home/more-about-us.vue";
-import footerBar from "../components/Home/footer.vue";
-import Navbar from "../components/Home/Navbar.vue";
-import VisionCarousel from "../components/Home/VisionCarousel.vue";
-import Webinar from "../components/Home/Webinar.vue";
-
-export default {
-  name: "home",
-  components: {
-    "card-bar": cardBar,
-    "popular-tech": popularTech,
-    "our-branches": ourBranches,
-    "more-about": moreAbout,
-    "footer-bar": footerBar,
-    "nav-bar": Navbar,
-    // "vision-carousel": VisionCarousel,
-    "webinar": Webinar,
-    "carousel-vis": VisionCarousel
+  export default {
+    components: {
+      HelloWorld
+    }
   }
-};
 </script>
