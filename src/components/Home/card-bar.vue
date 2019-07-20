29a1c66278de701bedb73676c4e51caaba4f2dc0
Shrikant S. C <chambharess16.it@coep.ac.in>
2019-07-20 20:39:32 +0530

    card bar is done using vue-awesome-swiper

--- src/components/Home/card-bar.vue
+++ src/components/Home/card-bar.vue
@@ -1,83 +1,84 @@
-<template lang="en">
-    <b-container fluid style="background-color:#2A326D; padding: 20px;">
-    <vueper-slides
-        class="no-shadow"
-      :visible-slides="4"
-      :slide-ratio="2/11"
-      :pauseOnHover="true"
-      :speed="1000"
-      :dragging-distance="50"
-      :autoplay="false"
-      :disableArrowsOnEdges="true"
-      :arrows="false"
-      :bullets="false"
-      :breakpoints="{
-        800: { visibleSlides: 1},
-        1025: { visibleSlides: 3},
-        450: { visibleSlides: 1}
-      }"
-    > <vueper-slide :key="1" class="card-container">
-            <div  slot="slideContent" style="padding: none;">
-                <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
-                <span>
-                    {{text}}     
-                </span>
+  
+<template>
+<b-container fluid style="background-color:#2A326D; padding: 20px;">
+
+      <!-- swiper -->
+      <swiper :options="swiperOption">
+        <swiper-slide >
+            <div class="card-container">
+            <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
+            <span>
+                {{text}}     
+            </span>
             </div>
-        </vueper-slide>
-        <vueper-slide :key="2" class="card-container">
-            <div  slot="slideContent" style="padding: none;">
-                <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
-                <span>
-                    {{text}}     
-                </span>
+        </swiper-slide>
+        <swiper-slide >
+            <div class="card-container">
+            <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
+            <span>
+                {{text}}     
+            </span>
             </div>
-        </vueper-slide>
-             <vueper-slide :key="3" class="card-container">
-            <div  slot="slideContent" style="padding: none;">
-                <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
-                <span>
-                    {{text}}     
-                </span>
+        </swiper-slide>
+        <swiper-slide >
+            <div class="card-container">
+            <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
+            <span>
+                {{text}}     
+            </span>
             </div>
-            </vueper-slide>
-             <vueper-slide :key="4" class="card-container">
-            <div  slot="slideContent" style="padding: none;">
-                <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
-                <span>
-                    {{text}}     
-                </span>
+        </swiper-slide>        
+        <swiper-slide >
+            <div class="card-container">
+            <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
+            <span>
+                {{text}}     
+            </span>
             </div>
-            </vueper-slide>
-             <vueper-slide :key="5" class="card-container">
-            <div  slot="slideContent" style="padding: none;">
-                <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
-                <span>
-                    {{text}}     
-                </span>
+        </swiper-slide> 
+        <swiper-slide >
+            <div class="card-container">
+            <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
+            <span>
+                {{text}}     
+            </span>
             </div>
-            </vueper-slide>
-             <vueper-slide :key="6" class="card-container">
-            <div  slot="slideContent" style="padding: none;">
-                <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
-                <span>
-                    {{text}}     
-                </span>
+        </swiper-slide> 
+        <swiper-slide >
+            <div class="card-container">
+            <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
+            <span>
+                {{text}}     
+            </span>
             </div>
-            </vueper-slide>
-        </vueper-slides>
-    </b-container>
+        </swiper-slide> 
+        <swiper-slide >
+            <div class="card-container">
+            <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
+            <span>
+                {{text}}     
+            </span>
+            </div>
+        </swiper-slide> 
+        <swiper-slide >
+            <div class="card-container">
+            <b-img-lazy class="card-container-img" v-bind="mainProps" :src="require('@/assets/images/profile_img_for_cardbar.png')"></b-img-lazy>
+            <span>
+                {{text}}     
+            </span>
+            </div>
+        </swiper-slide> 
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+</b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import { VueperSlides, VueperSlide } from "vueperslides";
-import "vueperslides/dist/vueperslides.css";
-export default {
-  name: "card-bar",
-  components: { VueperSlides, VueperSlide },
-  data() {
-    return {
-      mainProps: {
+
+  export default {
+    data() {
+      return {
+        mainProps: {
         center: true,
         rounded: "circle",
         fluidGrow: false,
@@ -85,33 +86,52 @@ export default {
         blankColor: "#bbb",
         alt: "Profile Picture"
       },
-      text:
-        "You know you're in love when you can't fall asleep because reality is finally better than your dreams. You only live once, but if you do it right, once is enough."
-    };
-  },
-  methods: {}
-};
+      text:"You know you're in love when you can't fall asleep because reality is finally better than your dreams. You only live once, but if you do it right, once is enough.", 
+        swiperOption: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+          // init: false,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          },
+          breakpoints: {
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 40
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 30
+            },
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20
+            },
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 10
+            }
+          }
+        }
+      }
+    }
+  }
 </script>
-
 <style scoped>
 .card-container {
   background-color: #eceef8;
   border: #eceef8;
   border-radius: 12px;
   color: #0e6251;
+  font-size: 12px;
   font-weight: bold;
-  text-align: unset;
-  width: 100px;
-  margin-right: 40px;
-  overflow: auto;
+  text-align: center;
+  padding: 4px;
 }
 .card-container-img {
   width: 68px;
   height: 66px;
 }
-@media only screen (max-width: 400px;){
-    .card-container{
-        margin-right: 0;
-    }
-}
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Vuetify from 'vuetify'
+import Vuetify from 'vuetify';
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
+// require styles
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper, /* { default global options } */)
 Vue.use(Vuetify)
 Vue.use(BootstrapVue);
 Vue.component('GoogleMap', GoogleMap);
