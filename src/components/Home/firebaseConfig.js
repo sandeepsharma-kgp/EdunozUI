dacf3d382431eba5fcdbae12c344a3fc038507ac
gauravtirodkar <gauravtirodkar42@gmail.com>
2019-07-26 19:52:15 +0530

    SignUp Signin not working

---
+++ src/components/Home/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyCVfvwhL_I9syRjYbhZ0Pk9egifhgkuDog",
+    authDomain: "eduauth-826a4.firebaseapp.com",
+    databaseURL: "https://eduauth-826a4.firebaseio.com",
+    projectId: "eduauth-826a4",
+    storageBucket: "eduauth-826a4.appspot.com",
+    messagingSenderId: "325003081008",
+    appId: "1:325003081008:web:489f32c87f0390d4"
+}
