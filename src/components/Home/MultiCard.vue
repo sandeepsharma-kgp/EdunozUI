a06cba17fa0c5db21f358ae2f109f8f3cda2b3af
gauravtirodkar <gauravtirodkar42@gmail.com>
2019-07-27 11:59:43 +0530

    Vision fixed. Carousel height adjusted. Multicard corrected

--- src/components/Home/MultiCard.vue
+++ src/components/Home/MultiCard.vue
@@ -1,26 +1,51 @@
 <template>
   <md-card> 
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
       <h1 align="center">Programs we Offer</h1>
     <md-card-actions>
     </md-card-actions>
     <md-card-media>
       <!-- swiper -->
       <swiper :options="swiperOption">
-        <swiper-slide ><b-img :src="require('../../assets/images/online_tests.jpg')" style="padding: 28px; width: 90%; height: 80%; 
+        <swiper-slide ><b-img :src="require('../../assets/images/adult-connection-data-1181354.jpg')" style="padding: 28px; width: 90%; height: 80%; 
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
-        <swiper-slide ><b-img :src="require('../../assets/images/get_full_support.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        <swiper-slide ><b-img :src="require('../../assets/images/aerial-desk-device-1011329.jpg')" style="padding: 28px; width: 90%; height: 80%;
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
-        <swiper-slide ><b-img :src="require('../../assets/images/get_certified.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        <swiper-slide ><b-img :src="require('../../assets/images/cheerful-colleagues-digital-device-1432942.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/adult-asian-caucasian-1153213.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/ballpen-blur-close-up-461077.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/code-coding-connection-943096.jpg')" style="padding: 28px; width: 90%; height: 80%;
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
         <swiper-slide ><b-img :src="require('../../assets/images/art-artsy-bulb-1938318.jpg')" style="padding: 28px; width: 90%; height: 80%;
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/businessmen-classroom-communication-267507.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
         <swiper-slide ><b-img :src="require('../../assets/images/get_certified.jpg')" style="padding: 28px; width: 90%; height: 80%;
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
         <swiper-slide ><b-img :src="require('../../assets/images/online_tests.jpg')" style="padding: 28px; width: 90%; height: 80%;
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
-        <swiper-slide ><b-img :src="require('../../assets/images/online_tests.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        <swiper-slide ><b-img :src="require('../../assets/images/adult-connection-data-1181354.jpg')" style="padding: 28px; width: 90%; height: 80%; 
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
-        <swiper-slide ><b-img :src="require('../../assets/images/get_full_support.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        <swiper-slide ><b-img :src="require('../../assets/images/aerial-desk-device-1011329.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/cheerful-colleagues-digital-device-1432942.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/adult-asian-caucasian-1153213.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/ballpen-blur-close-up-461077.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/code-coding-connection-943096.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/art-artsy-bulb-1938318.jpg')" style="padding: 28px; width: 90%; height: 80%;
+        margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
+        <swiper-slide ><b-img :src="require('../../assets/images/businessmen-classroom-communication-267507.jpg')" style="padding: 28px; width: 90%; height: 80%;
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
         <swiper-slide ><b-img :src="require('../../assets/images/get_certified.jpg')" style="padding: 28px; width: 90%; height: 80%;
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
@@ -28,6 +53,8 @@
         margin-right: auto; margin-left: auto;"></b-img></swiper-slide>
        
         <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
       </swiper>
     </md-card-media>
   </md-card>
@@ -44,6 +71,10 @@
           pagination: {
             el: '.swiper-pagination',
             clickable: true
+          },
+           navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
           },
           breakpoints: {
             1024: {
